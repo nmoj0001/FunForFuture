c8a7036f232e72c90c08c7f019789f0147af7011
SraboniMojumder <nusrat.mojumder.1995@gmail.com>
2019-08-25 11:43:11 +1000

    Added sortyourwaste game config

---
+++ game1/js/Game.js
@@ -0,0 +1,17 @@
+var config = {
+    type: Phaser.AUTO,
+    scale: {
+        parent: 'phaser-example',
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1920,
+        height: 1080,
+    },
+    physics: {
+        default: 'matter',
+        matter: { debug : true }
+    },
+    scene: [ Level1 ]
+};
+
+var game = new Phaser.Game(config);
