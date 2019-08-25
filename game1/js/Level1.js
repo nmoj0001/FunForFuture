b498d0319d23f484670384b7e68c4fc3602e4c57
SraboniMojumder <nusrat.mojumder.1995@gmail.com>
2019-08-26 02:30:16 +1000

    update collides with for waste object

--- game1/js/Level1.js
+++ game1/js/Level1.js
@@ -47,20 +47,26 @@ var Level1 = new Phaser.Class({
         this.add.image(800, 600, 'garbage').setOrigin(0);
         this.add.image(1100, 600, 'recycling').setOrigin(0);
 
-        organic = this.matter.add.image(530, 750, 'rectangle', null, { isStatic : true }).setOrigin(0);
+        organic = this.matter.add.image(530, 750, 'rectangle', null, { isStatic : true });
         organic.setCollisionCategory(constants.ORGANIC_BIN_COLLISION_CATEGORY);
         organic.setVisible(false);
 
-        garbage = this.matter.add.image(830, 750, 'rectangle', null, { isStatic : true }).setOrigin(0);
+        garbage = this.matter.add.image(830, 750, 'rectangle', null, { isStatic : true });
         garbage.setCollisionCategory(constants.GARBAGE_BIN_COLLISION_CATEGORY);
         garbage.setVisible(false);
 
-        recycling = this.matter.add.image(1130, 750, 'rectangle', null, { isStatic : true }).setOrigin(0);
+        recycling = this.matter.add.image(1130, 750, 'rectangle', null, { isStatic : true });
         recycling.setCollisionCategory(constants.RECYCLING_BIN_COLLISION_CATEGORY);
         recycling.setVisible(false);
 
         apple = this.matter.add.sprite(900, 150, 'o_apple', null, { isStatic : true }).setInteractive();
         apple.setCollisionCategory(constants.ORGANIC_COLLISION_CATEGORY);
+        apple.setCollidesWith([
+            constants.GARBAGE_BIN_COLLISION_CATEGORY,
+            constants.ORGANIC_BIN_COLLISION_CATEGORY,
+            constants.RECYCLING_BIN_COLLISION_CATEGORY
+        ]);
+        
         this.dragObject(apple);
     },
 
