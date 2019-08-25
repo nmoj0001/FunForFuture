03ba1e77328ce732925ed9c094246ff8b9d93273
SraboniMojumder <nusrat.mojumder.1995@gmail.com>
2019-08-25 14:17:33 +1000

    update world bound

--- game1/js/Level1.js
+++ game1/js/Level1.js
@@ -34,7 +34,7 @@ var Level1 = new Phaser.Class({
     },
 
     setUp: function (){
-        // this.matter.world.setBounds(0, 0, 1920, 1080);
+        this.matter.world.setBounds(0, 0, 1920, 1080);
         this.add.image(0, 0, 'level1_background').setOrigin(0);
 
     }
