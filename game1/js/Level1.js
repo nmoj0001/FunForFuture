d39e00a0e06357b1f25db3a4f924bb5b943e8622
SraboniMojumder <nusrat.mojumder.1995@gmail.com>
2019-08-25 16:22:04 +1000

    update image

--- game1/js/Level1.js
+++ game1/js/Level1.js
@@ -37,8 +37,8 @@ var Level1 = new Phaser.Class({
         this.matter.world.setBounds(0, 0, 1920, 1080);
         this.add.image(0, 0, 'level1_background').setOrigin(0);
         this.add.image(400, 600, 'organic').setOrigin(0);
-        this.add.image(900, 600, 'garbage').setOrigin(0);
-        this.add.image(1200, 600, 'recycling').setOrigin(0);
+        this.add.image(700, 600, 'garbage').setOrigin(0);
+        this.add.image(1000, 600, 'recycling').setOrigin(0);
         // this.add.image(550, 800, 'o_apple').setOrigin(0);
         // this.add.image(700, 800, 'o_bananapeel').setOrigin(0);
         // this.add.image(250, 900, 'r_tincan').setOrigin(0);
