9fa25f1be1a88191af7cc0b9a4747f9f6ef4438d
SraboniMojumder <nusrat.mojumder.1995@gmail.com>
2019-08-25 21:39:17 +1000

    test

--- game1/js/Level1.js
+++ game1/js/Level1.js
@@ -1,93 +1,53 @@
-var apple;
-var bananapeel;
-var cereal;
-var chips;
-var plasticbottle;
-var tincan;
-var pointer;
-var score;
+var config = {
+    type: Phaser.WEBGL,
+    parent: 'phaser-example',
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
 
-var Level1 = new Phaser.Class({
+var game = new Phaser.Game(config);
 
-    Extends: Phaser.Scene,
+function preload ()
+{
+    this.load.image('eye', 'assets/waste/apple.png');
+}
 
-    initialize:
-        function Level1() {
-            Phaser.Scene.call(this, { key : 'Level1' });
-        },
+function create ()
+{
+    var image = this.add.sprite(200, 300, 'eye').setInteractive();
 
-    preload: function() {
-        this.loadAssets();
-    },
+    image.on('pointerover', function () {
 
-    create: function (){
-        this.setUp();
-    },
+        this.setTint(0x00ff00);
 
-    loadAssets: function() {
-        this.load.image('organic', 'assets/bins/organic.png');
-        this.load.image('garbage', 'assets/bins/garbage.png');
-        this.load.image('recycling', 'assets/bins/recycling.png');
-        this.load.image('organic', 'assets/bins/organic.png');
-        this.load.image('o_apple', 'assets/waste/apple.png');
-        this.load.image('o_bananapeel', 'assets/waste/bananapeel.png');
-        this.load.image('r_plasticbottle', 'assets/waste/plasticbottle.png');
-        this.load.image('r_cereal', 'assets/waste/cereal.png');
-        this.load.image('r_tincan', 'assets/waste/tincan.png');
-        this.load.image('g_chips', 'assets/waste/chips.png');
-        this.load.image('level1_background', 'assets/level1_background.jpg');
-    },
+    });
 
-    setUp: function (){
-        this.matter.world.setBounds(0, 0, 1920, 1080);
-        this.add.image(0, 0, 'level1_background').setOrigin(0);
-        this.add.image(500, 600, 'organic').setOrigin(0);
-        this.add.image(800, 600, 'garbage').setOrigin(0);
-        this.add.image(1100, 600, 'recycling').setOrigin(0);
-        // apple = this.add.image(550, 800, 'o_apple').setOrigin(0);
-        // bananapeel = this.add.image(700, 800, 'o_bananapeel').setOrigin(0);
-        // tincan = this.add.image(250, 900, 'r_tincan').setOrigin(0);
-        // plasticbottle = this.add.image(350, 800, 'r_plasticbottle').setOrigin(0); 
-        // chips = this.add.image(500, 950, 'g_chips').setOrigin(0);
-        
-        apple = this.add.sprite(850, 150, 'o_apple').setOrigin(0);
-        apple.setInteractive();
-        image.on('pointerover', function () {
+    image.on('pointerout', function () {
 
-            this.setTint(0x00ff00);
-    
-        });
-    
-        image.on('pointerout', function () {
-    
-            this.clearTint();
-    
-        });
-    
-        this.input.setDraggable(image);
-    
-        this.input.on('dragstart', function (pointer, gameObject) {
-    
-            gameObject.setTint(0xff0000);
-    
-        });
-    
-        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-    
-            gameObject.x = dragX;
-            gameObject.y = dragY;
-    
-        });
-    
-        this.input.on('dragend', function (pointer, gameObject) {
-    
-            gameObject.clearTint();
-    
-        });
+        this.clearTint();
 
-    },
+    });
 
-    update: function (){
+    this.input.setDraggable(image);
 
-    },
-});
+    this.input.on('dragstart', function (pointer, gameObject) {
+
+        gameObject.setTint(0xff0000);
+
+    });
+
+    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+
+        gameObject.x = dragX;
+        gameObject.y = dragY;
+
+    });
+
+    this.input.on('dragend', function (pointer, gameObject) {
+
+        gameObject.clearTint();
+
+    });
+}
