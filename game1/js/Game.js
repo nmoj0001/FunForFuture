8d5d8f899fc08d9d2c16d5d0e936a8e97a1beda4
SraboniMojumder <nusrat.mojumder.1995@gmail.com>
2019-08-25 13:43:54 +1000

    change in config

--- game1/js/Game.js
+++ game1/js/Game.js
@@ -2,8 +2,8 @@ var config = {
     type: Phaser.AUTO,
     scale: {
         parent: 'phaser-example',
-        // mode: Phaser.Scale.FIT,
-        // autoCenter: Phaser.Scale.CENTER_BOTH,
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
         width: 1920,
         height: 1080,
     },
