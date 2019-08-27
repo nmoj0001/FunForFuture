32e80fd1e8ea2e0cd5fa1b0c52cf3c2c8c7bf32c
SraboniMojumder <nusrat.mojumder.1995@gmail.com>
2019-08-27 18:16:51 +1000

    update Enter key code

--- game1/js/StartScreen.js
+++ game1/js/StartScreen.js
@@ -1,6 +1,5 @@
 var startScreenBg;
 var gameLogo;
-var enterKey;
 
 var StartScreen = new Phaser.Class({
 
@@ -18,13 +17,12 @@ var StartScreen = new Phaser.Class({
     },
 
     create: function () {
-        enterKey = game.input.keyboard(Phaser.Input.Keyboard.ENTER);
         startScreenBg = this.add.image(0, 0, 'level1_background').setOrigin(0);
         gameLogo = this.matter.add.sprite(game.scale.width / 2, game.scale.height / 2, 'game_logo', null, { isStatic: true });
     },
 
     update: function () {
-        if (enterKey.isDown) {
+        if (Phaser.Input.Keyboard.JustDown(Phser.Input.Keyboard.KeyCodes.ENTER)) {
             startGame();
         }
     },
