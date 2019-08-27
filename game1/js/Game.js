ba5eeaf094cf0299535281b8ee92131e620a5003
SraboniMojumder <nusrat.mojumder.1995@gmail.com>
2019-08-27 18:01:31 +1000

    test baselevel

--- game1/js/Game.js
+++ game1/js/Game.js
@@ -11,7 +11,7 @@ var config = {
         default: 'matter',
         matter: { debug : true }
     },
-    scene: [ StartScreen ]
+    scene: [ Baselevel ]
 };
 
 var game = new Phaser.Game(config);
