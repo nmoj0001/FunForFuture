85191a164bdb1f5b95e471c67c8697977310394c
jeyganesh7 <jtir0001@student.monash.edu>
2019-09-07 18:37:06 +1000

    ajax database

---
+++ iteration2/databasefact.php
@@ -0,0 +1,24 @@
+<?php
+
+$dbServername = "ec2-18-218-62-68.us-east-2.compute.amazonaws.com";
+$dbUsername = "root";
+$dbPassword = "jeyganesh";
+$dbName = "waste";
+
+$mysqli = new mysqli("dbServername", "dbUsername", "dbPassword", "dbName");
+if($mysqli->connect_error) {
+  exit('Could not connect');
+}
+
+$sql = "SELECT factdescription FROM facts ORDER BY RAND() LIMIT 1";
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("s", $_GET['q']);
+$stmt->execute();
+$stmt->store_result();
+$stmt->bind_result($factdescription);
+$stmt->fetch();
+$stmt->close();
+
+echo $factdescription;
+
+?>
