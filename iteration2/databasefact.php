93c547f72741037814ef3f2807ec30d6c8b2cccc
jeyganesh7 <jtir0001@student.monash.edu>
2019-09-13 02:37:03 +1000

    database update8

--- iteration2/databasefact.php
+++ iteration2/databasefact.php
@@ -11,20 +11,20 @@ if($mysqli->connect_error) {
   exit('Could not connect');
 }
 
-//$sql = "SELECT fact_description, fact_source FROM fact ORDER BY RAND(" . date("Ymd") . ") LIMIT 1";
-$sql = "SELECT fact_description FROM Fact ORDER BY RAND() LIMIT 1";
+//$sql = "SELECT fact_description, fact_source FROM Fact ORDER BY RAND(" . date("Ymd") . ") LIMIT 1";
+//$sql = "SELECT fact_description FROM Fact ORDER BY RAND() LIMIT 1";
 //$sql = "Select fact_description, fact_source from Fact";
 $stmt = $mysqli->prepare($sql);
 $stmt->bind_param("s", $_GET['q']);
 $stmt->execute();
 $stmt->store_result();
-$stmt->bind_result($fact_description);
+$stmt->bind_result($fact_description,$fact_source);
 $stmt->fetch();
 $stmt->close();
 
 echo "Fact";
 echo "<h3>".$fact_description."</h3>";
-//echo "Source";
-//echo "<h4>".$fact_source."</h4>";
+echo "Source";
+echo "<h4>".$fact_source."</h4>";
 
 ?>
