5704eb8af7c700636f0ec18760fcebd178803b77
jeyganesh7 <jtir0001@student.monash.edu>
2019-09-13 02:45:49 +1000

    database update9

--- iteration2/databasefact.php
+++ iteration2/databasefact.php
@@ -14,17 +14,24 @@ if($mysqli->connect_error) {
 //$sql = "SELECT fact_description, fact_source FROM Fact ORDER BY RAND(" . date("Ymd") . ") LIMIT 1";
 //$sql = "SELECT fact_description FROM Fact ORDER BY RAND() LIMIT 1";
 //$sql = "Select fact_description, fact_source from Fact";
-$stmt = $mysqli->prepare($sql);
-$stmt->bind_param("s", $_GET['q']);
-$stmt->execute();
-$stmt->store_result();
-$stmt->bind_result($fact_description,$fact_source);
-$stmt->fetch();
-$stmt->close();
+//$stmt = $mysqli->prepare($sql);
+//$stmt->bind_param("s", $_GET['q']);
+//$stmt->execute();
+//$stmt->store_result();
+//$stmt->bind_result($fact_description,$fact_source);
+//$stmt->fetch();
+//$stmt->close();
+
+$sql = "SELECT fact_description, fact_source FROM Fact ORDER BY RAND(" . date("Ymd") . ") LIMIT 1";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
 
 echo "Fact";
-echo "<h3>".$fact_description."</h3>";
+echo "".$row["fact_description"]."";
 echo "Source";
-echo "<h4>".$fact_source."</h4>";
+echo "<h3>".$row["fact_source"]."</h3>";
+
+//echo "Source";
+//echo "<h4>".$fact_source."</h4>";
 
 ?>
