76cb14f3e90c62763fa7ee90038fb24590b8903d
jeyganesh7 <jtir0001@student.monash.edu>
2019-09-13 17:47:23 +1000

    quiz game14

--- iteration2/sample.php
+++ iteration2/sample.php
@@ -8,9 +8,23 @@ $row1 = $result->fetch_assoc();
 
 echo $row1["sno"]; 
 //echo $row1["name"];
-echo "data: $mime".$row1["name"];
+echo '<img src="data:image/jpeg;base64,'.base64_encode($row1["name"]->load()) .'" />';
 
+/*
+
+echo '<img src="data:name/jpeg;base64,'.base64_encode($row1["name"] ).'" height="200" width="200"/>';
+(isset($_POST["submit2"]))
+
+$res=mysqli_query($conn,"select * from sample");
+  
+$row=mysqli_fetch_array($res);
+echo "quiz game";
+echo $row['sno']; 
+ echo '<img src="data:name/jpeg;base64,'.base64_encode($row['name']).'" height="200" width="200"/>';
+   
+*/
 ?>
+
 
 
 
