2970861703e9b2a7858c90575046f0e47456df8a
jeyganesh7 <jtir0001@student.monash.edu>
2019-09-13 16:58:55 +1000

    quiz game14

--- iteration2/sample.php
+++ iteration2/sample.php
@@ -2,20 +2,20 @@
 
 <?php
 
-//$sql = "SELECT * FROM sample LIMIT 1";
-//$result = $conn->query($sql);
-//$row1 = $result->fetch_assoc();
+$sql = "SELECT sno,name FROM sample LIMIT 1";
+$result = $conn->query($sql);
+$row1 = $result->fetch_assoc();
 <h3>"Is this recyclable?"</h3>;
-//echo " ".$row1["sno"]." "; 
+echo " ".$row1["sno"]." "; 
 //echo '<img src="data:name/jpeg;base64,'.base64_encode($row1['name'] ).'" height="200" width="200"/>';
 //(isset($_POST["submit2"]))
 
-$res=mysqli_query($conn,"select * from sample");
+//$res=mysqli_query($conn,"select * from sample");
   
-$row=mysqli_fetch_array($res);
-echo "quiz game";
-   echo $row['sno']; 
-  // echo '<img src="data:name/jpeg;base64,'.base64_encode($row['name']).'" height="200" width="200"/>';
+//$row=mysqli_fetch_array($res);
+//echo "quiz game";
+//echo $row['sno']; 
+// echo '<img src="data:name/jpeg;base64,'.base64_encode($row['name']).'" height="200" width="200"/>';
    
 ?>
 
