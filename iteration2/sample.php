80334381183d854cec79f8d6b87fb30d5e44dadd
jeyganesh7 <jtir0001@student.monash.edu>
2019-09-13 17:02:58 +1000

    quiz game14

--- iteration2/sample.php
+++ iteration2/sample.php
@@ -1,12 +1,12 @@
 <?php include("dbconnection.php"); ?> 
-
+<h3>"Is this recyclable?"</h3>;
 <?php
 
 $sql = "SELECT sno FROM sample LIMIT 1";
 $result = $conn->query($sql);
 $row1 = $result->fetch_assoc();
-<h3>"Is this recyclable?"</h3>;
-echo " ".$row1["sno"]." "; 
+
+echo $row1["sno"]; 
 //echo '<img src="data:name/jpeg;base64,'.base64_encode($row1['name'] ).'" height="200" width="200"/>';
 //(isset($_POST["submit2"]))
 
