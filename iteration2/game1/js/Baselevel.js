dc4601fc8bff79b8bee0019977c43ce25a6b0a2d
nmoj0001 <nmoj0001@student.monash.edu>
2019-09-18 07:05:55 +1000

    update asset

--- iteration2/game1/js/Baselevel.js
+++ iteration2/game1/js/Baselevel.js
@@ -74,7 +74,7 @@ var Baselevel = new Phaser.Class({
         garbage.setScale(.7);
         recycling.setScale(.7);
 
-        girl = this.add.image(150, 300, 'girl').setOrigin(0);
+        girl = this.add.image(150, 400, 'girl').setOrigin(0);
         speechBubble = this.add.image(300, 200, 'speechBubble').setOrigin(0);
 
         score = 0;
