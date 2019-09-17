2a5b0877089eda66c26994ce9da1a39bf042178e
nmoj0001 <nmoj0001@student.monash.edu>
2019-09-18 07:20:15 +1000

    update text

--- iteration2/game1/js/Baselevel.js
+++ iteration2/game1/js/Baselevel.js
@@ -77,7 +77,7 @@ var Baselevel = new Phaser.Class({
 
         girl = this.add.image(50, 420, 'girl').setOrigin(0);
         speechBubble = this.add.image(150, 100, 'speechBubble').setOrigin(0);
-        speechBubbleText = this.add.text(250, 200, 'Welcome to Sort Your Waste', { font: "40px Arial Black", fill: "#fff" }).setStroke('#ffdd00', 16).setShadow(2, 2, "#333333", 2, true, true);
+        speechBubbleText = this.add.text(250, 100, 'Welcome\nto\nSort Your Waste', { font: "40px Arial Black", fill: "#fff" }).setStroke('#ffdd00', 16).setShadow(2, 2, "#333333", 2, true, true);
        
 
         score = 0;
