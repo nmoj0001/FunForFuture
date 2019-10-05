e6d3d9b668626a8bad444df5ab3627d82a0828a5
jeyganesh7 <54430564+jeyganesh7@users.noreply.github.com>
2019-10-05 16:16:09 +1000

    signin page

--- iteration3/auth.php
+++ iteration3/auth.php
@@ -18,8 +18,7 @@ if (!empty($username) || !empty($psw)) {
      session_start();
   $_SESSION['logged']=true;
   $_SESSION ['username']=$username;
-  $message = $username;
-  echo '<script type= "text/JavaScript"> alert('$message'); </script>';
+  echo '<script type= "text/JavaScript"> alert('$_SESSION ['username']'); </script>';
     header("refresh:1;url=index1.html");
 	
      }
