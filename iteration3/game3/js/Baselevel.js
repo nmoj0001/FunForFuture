df0b743f013d6918ea902a03d2e51486d56713bc
SraboniMojumder <nusrat.mojumder.1995@gmail.com>
2019-10-08 17:32:40 +1100

    Add Player

--- iteration3/game3/js/Baselevel.js
+++ iteration3/game3/js/Baselevel.js
@@ -20,9 +20,9 @@ var Baselevel = new Phaser.Class({
     this.load.image('platform_long', 'assets/common/platform_long.png');
     this.load.image('platform_medium', 'assets/common/platform_medium.png');
     this.load.image('platform_short', 'assets/common/platform_short.png');
-    this.load.spritesheet('girl', 
-        'assets/common/girl.png',
-        { frameWidth: 32, frameHeight: 48 }
+    this.load.spritesheet('girl',
+      'assets/common/girl.png',
+      { frameWidth: 32, frameHeight: 48 }
     );
   },
 
@@ -32,6 +32,7 @@ var Baselevel = new Phaser.Class({
 
     background = this.add.image(0, 0, 'background').setOrigin(0);
     recycle = this.add.image(0, 0, 'recycle').setOrigin(0);
+    this.createPlayer();
 
     // scoreImage = this.add.image(1750, 70, 'score');
     // scoreText = this.add
@@ -40,6 +41,33 @@ var Baselevel = new Phaser.Class({
     //   .setShadow(2, 2, '#333333', 2, true, true);
 
     // this.updateScore();
+  },
+
+  createPlayer: function () {
+    player = this.physics.add.sprite(1000, 800, 'girl');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('girl', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'turn',
+      frames: [{ key: 'girl', frame: 4 }],
+      frameRate: 20
+    });
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('girl', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+    });
   },
 
   update: function () { },
