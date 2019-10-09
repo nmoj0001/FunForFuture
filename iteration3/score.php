585746fab016935ab412bbb386441fe0c29c4600
jeyganesh7 <54430564+jeyganesh7@users.noreply.github.com>
2019-10-10 03:31:43 +1100

    submitscore1

--- iteration3/score.php
+++ iteration3/score.php
@@ -6,13 +6,21 @@ session_start();
 if(isset($_SESSION['username']))
 {
 //conversion
-$game =	$_SESSION['gameID'];
-$user = $_SESSION['username'];	
-$score = $_SESSION['score'];
+//$game =	$_SESSION['gameID'];
+//$user = $_SESSION['username'];	
+//$score = $_SESSION['score'];
+$game =	1;
+$user = 'ganesh';	
+$score = 1234;
 
+	 //$SELECT = "SELECT username From jeyganesh.score Where username = '".$user."' and gameID = $game";
+     //$INSERT = "INSERT Into jeyganesh.score (gameID,username,gameScore) values($game, $user, $score)";
+	 //$UPDATE = "UPDATE jeyganesh.score SET gameScore = $score where username = '".$user."' and gameID = $game";
+	 
 	 $SELECT = "SELECT username From jeyganesh.score Where username = $user and gameID = $game";
      $INSERT = "INSERT Into jeyganesh.score (gameID,username,gameScore) values($game, $user, $score)";
 	 $UPDATE = "UPDATE jeyganesh.score SET gameScore = $score where username = $user and gameID = $game";
+	 
      //Prepare statement
      $stmt = $conn->prepare($SELECT);
      $stmt->bind_param("s", $user);
@@ -25,15 +33,15 @@ $score = $_SESSION['score'];
       $stmt = $conn->prepare($INSERT);
       $stmt->bind_param("isi", $game, $user, $score);
       $stmt->execute();
-      //echo '<script type= "text/JavaScript"> alert("Signup successful"); </script>';
-	  //include_once('sigin.html');
+      echo '<script type= "text/JavaScript"> alert("insert"); </script>';
+	  include_once('leaderboard.html');
      }
 	 else {
 	$stmt = $conn->prepare($UPDATE);
       $stmt->bind_param("isi", $game, $user, $score);
       $stmt->execute();
-     //echo '<script type= "text/JavaScript"> alert("Name already exists. Enter new name."); </script>';
-	 //include_once('signup.html');
+     echo '<script type= "text/JavaScript"> alert("update"); </script>';
+	 include_once('leaderboard.html');
      }
      $stmt->close();
      $conn->close();
