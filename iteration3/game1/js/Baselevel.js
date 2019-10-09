765e928c0cb88971703a8f6b092c9734b4fc25c6
nmoj0001 <nmoj0001@student.monash.edu>
2019-10-10 10:15:41 +1100

    tween test

--- iteration3/game1/js/Baselevel.js
+++ iteration3/game1/js/Baselevel.js
@@ -105,14 +105,14 @@ var Baselevel = new Phaser.Class({
     else if (num >= 11 && num <= 15) category = 'garbage';
 
     waste = this.matter.add
-      .image(1000, 250, 'l' + level + '_' + num, null, { isStatic: true })
+      .image(1050, 250, 'l' + level + '_' + num, null, { isStatic: true })
       .setInteractive();
 
       var tween = this.tweens.add({
         targets: waste,
-        x: 100,
+        x: 20,
         ease: 'Power1',
-        duration: 400,
+        duration: 600,
         yoyo: true,
         repeat: 1,
     });
