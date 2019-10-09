1f190e485d5ef2ffc7252a326b08521b299d9f71
nmoj0001 <nmoj0001@student.monash.edu>
2019-10-10 06:59:32 +1100

    Add level text

--- iteration3/game3/js/Level1.js
+++ iteration3/game3/js/Level1.js
@@ -50,7 +50,7 @@ var Level1 = new Phaser.Class({
     dialogueBox.setScale(0.8)
     play = this.add.image(1020, 680, 'play');
     openingText = this.add
-      .text(680, 200, '     Some solid waste materials\n   cannot be disposed by putting\n               in kerbside bins.\n        These must be taken to\n Municipal solid waste collection.\n Collect these solid waste items\n  to keep the playground clean!\n', { font: '35px Arial Black', fill: '#fff' })
+      .text(680, 180, '          Level 1          \n     Some solid waste materials\n   cannot be disposed by putting\n               in kerbside bins.\n        These must be taken to\n Municipal solid waste collection.\n Collect these solid waste items\n  to keep the playground clean!\n', { font: '35px Arial Black', fill: '#fff' })
       .setStroke('#ffc812', 16)
       .setShadow(2, 2, '#333333', 2, true, true);
     this.startLevel();
