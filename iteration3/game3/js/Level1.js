40544d3b2b78cc6c0118c6abcdd3ed7e0c0cbef4
nmoj0001 <nmoj0001@student.monash.edu>
2019-10-10 06:56:26 +1100

    fix position

--- iteration3/game3/js/Level1.js
+++ iteration3/game3/js/Level1.js
@@ -29,7 +29,7 @@ var Level1 = new Phaser.Class({
     this.createPlayer();
     this.setOpening();
     instructions = this.add.image(1000, 450, 'instructions');
-    resume = this.add.image(1000, 650, 'resume');
+    resume = this.add.image(1000, 750, 'resume');
     this.resumeGame();
   },
 
