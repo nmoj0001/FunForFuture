c0a7f5e68bed4de19aa681853696924a58bc9ad4
lee <lee.lonny@gmail.com>
2019-10-10 14:52:06 +1100

    signinlogin

--- iteration3/auth.php
+++ iteration3/auth.php
@@ -8,38 +8,34 @@ $username = $_POST['username'];
 $psw = $_POST['psw'];
 if (!empty($username) || !empty($psw)) {
 
-     $SELECT = "SELECT username, psw From jeyganesh.user Where username = ? and psw = ? Limit 1";
-     //$INSERT = "INSERT Into jeyganesh.user (username,psw) values(?, ?)";
-     //Prepare statement
-     $stmt = $conn->prepare($SELECT);
-     $stmt->bind_param("ss", $username, $psw);
-     $stmt->execute();
-     $stmt->bind_result($username);
-     $stmt->store_result();
-     $rnum = $stmt->num_rows;
-     if ($rnum==1) {
-     
-  $_SESSION['logged']=true;
-  $_SESSION ['username']=$username;
+    $SELECT = "SELECT username, psw From jeyganesh.user Where username = ? and psw = ? Limit 1";
+    //$INSERT = "INSERT Into jeyganesh.user (username,psw) values(?, ?)";
+    //Prepare statement
+    $stmt = $conn->prepare($SELECT);
+    $stmt->bind_param("ss", $username, $psw);
+    $stmt->execute();
+    $stmt->bind_result($username);
+    $stmt->store_result();
+    $rnum = $stmt->num_rows;
+    if ($rnum == 1) {
 
-	echo '<script type= "text/JavaScript"> alert("Welcome"); </script>';
-    header("refresh:1;url=index.html");
-	
-     }
-	 else {
-     echo '<script type= "text/JavaScript"> alert("Invalid username and psw"); </script>';
-	    $_SESSION['logged']=false;
-	header("refresh:2;url=signin.html");
-  
-	 //include_once('signin.html');
-	 //echo '<script type= "text/JavaScript"> alert("Welcome $username"); </script>';
-     }
-     $stmt->close();
-     $conn->close();
-    } 
+        $_SESSION['logged'] = true;
+        $_SESSION['username'] = $username;
 
-else {
- echo "All fields are required";
- die();
+        echo '<script type= "text/JavaScript"> alert("Welcome"); </script>';
+        header("refresh:1;url=index.html");
+    } else {
+        echo '<script type= "text/JavaScript"> alert("Invalid username and password"); </script>';
+        $_SESSION['logged'] = false;
+        header("refresh:2;url=login.html");
+
+        //include_once('signin.html');
+        //echo '<script type= "text/JavaScript"> alert("Welcome $username"); </script>';
+    }
+    $stmt->close();
+    $conn->close();
+} else {
+    echo "All fields are required";
+    die();
 }
 ?>
