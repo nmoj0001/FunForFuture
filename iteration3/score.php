1e2c2a7a68ecd0c98b70e40dbf125651126c94ca
jeyganesh7 <54430564+jeyganesh7@users.noreply.github.com>
2019-10-10 13:01:17 +1100

    submitscore3

--- iteration3/score.php
+++ iteration3/score.php
@@ -10,8 +10,9 @@ if(isset($_SESSION['username']))
 $user = $_SESSION['username'];	
 //$score = $_SESSION['score'];
 $game =	1;	
-$score = $_[POST];
-echo $_[POST];
+$score = $_POST['score'];
+echo '<script type= "text/JavaScript"> alert("hi"); </script>'; 
+//echo $_[POST];
 	 $SELECT = "SELECT username From jeyganesh.score Where username = '".$user."' and gameID = $game";
      $INSERT = "INSERT Into jeyganesh.score (gameID,username,gameScore) values($game, '".$user."', $score)";
 	 $UPDATE = "UPDATE jeyganesh.score SET gameScore = $score where username = '".$user."' and gameID = $game";
