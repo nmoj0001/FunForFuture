8e81acef388a093fde0f1b986a379406b8257e62
jeyganesh7 <54430564+jeyganesh7@users.noreply.github.com>
2019-10-13 23:16:16 +1100

    submitscore

--- score.php
+++ score.php
@@ -10,9 +10,9 @@ $score = 2323;
 
 	$SELECT = "SELECT username From jeyganesh.score Where username = 'rithika' and gameID = 1";
      $INSERT = "INSERT Into jeyganesh.score (gameID,username,gameScore) values(1, 'rithika', 2323)";
-	 $UPDATE = "UPDATE jeyganesh.score SET gameScore = 2222 where username = 'rithika' and gameID = 1";
+	$UPDATE = "UPDATE jeyganesh.score SET gameScore = 2222 where username = 'rithika' and gameID = 1";
 	 
-	 $stmt = $conn->prepare($SELECT);
+	$stmt = $conn->prepare($SELECT);
      $stmt->bind_param("s", rithika);
      $stmt->execute();
      $stmt->bind_result($user);
