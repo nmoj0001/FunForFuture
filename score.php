c9a32a06a0d358e52c1f37634e30f902ad2c6b8d
jeyganesh7 <54430564+jeyganesh7@users.noreply.github.com>
2019-10-13 23:20:16 +1100

    submitscore2

--- score.php
+++ score.php
@@ -12,30 +12,30 @@ $score = 2323;
      $INSERT = "INSERT Into jeyganesh.score (gameID,username,gameScore) values(1, 'rithika', 2323)";
 	$UPDATE = "UPDATE jeyganesh.score SET gameScore = 2222 where username = 'rithika' and gameID = 1";
 	 
-	$stmt = $conn->prepare($SELECT);
-     $stmt->bind_param("s", rithika);
-     $stmt->execute();
-     $stmt->bind_result($user);
-     $stmt->store_result();
-     $rnum = $stmt->num_rows;
-     if ($rnum==0) {
-		echo '<script type= "text/JavaScript"> alert("insert"); </script>'; 
-      $stmt->close();
-      $stmt = $conn->prepare($INSERT);
-      $stmt->bind_param("isi", $game, rithika, $score);
-      $stmt->execute();
-      echo '<script type= "text/JavaScript"> alert("insert"); </script>';
-	  include_once('leaderboard.html');
-     }
-	 else {
-	$stmt = $conn->prepare($UPDATE);
-      $stmt->bind_param("isi", $game, rithika, $score);
-      $stmt->execute();
-     echo '<script type= "text/JavaScript"> alert("update"); </script>';
-	 include_once('leaderboard.html');
-     }
-     $stmt->close();
-     $conn->close();
+	// $stmt = $conn->prepare($SELECT);
+     // $stmt->bind_param("s", rithika);
+     // $stmt->execute();
+     // $stmt->bind_result($user);
+     // $stmt->store_result();
+     // $rnum = $stmt->num_rows;
+     // if ($rnum==0) {
+	// 	echo '<script type= "text/JavaScript"> alert("insert"); </script>'; 
+     //  $stmt->close();
+     //  $stmt = $conn->prepare($INSERT);
+     //  $stmt->bind_param("isi", $game, rithika, $score);
+     //  $stmt->execute();
+     //  echo '<script type= "text/JavaScript"> alert("insert"); </script>';
+	//   include_once('leaderboard.html');
+     // }
+	//  else {
+	// $stmt = $conn->prepare($UPDATE);
+     //  $stmt->bind_param("isi", $game, rithika, $score);
+     //  $stmt->execute();
+     // echo '<script type= "text/JavaScript"> alert("update"); </script>';
+	//  include_once('leaderboard.html');
+     // }
+     // $stmt->close();
+     // $conn->close();
 
 }
 else
