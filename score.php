94cb38a1e82f111348496f1a559faec8282ff999
jeyganesh7 <54430564+jeyganesh7@users.noreply.github.com>
2019-10-13 23:40:30 +1100

    test 2

--- score.php
+++ score.php
@@ -11,24 +11,24 @@ $score = 2323;
 	$SELECT = "SELECT username From jeyganesh.score Where username = 'rithika' and gameID = 1";
      $INSERT = "INSERT Into jeyganesh.score (gameID,username,gameScore) values(1, 'rithika', 2323)";
 	$UPDATE = "UPDATE jeyganesh.score SET gameScore = 2222 where username = 'rithika' and gameID = 1";
-      
-     echo "Reached here";
 
-	// $stmt = $conn->prepare($SELECT);
-     // $stmt->bind_param("s", rithika);
-     // $stmt->execute();
-     // $stmt->bind_result($user);
-     // $stmt->store_result();
-     // $rnum = $stmt->num_rows;
-     // if ($rnum==0) {
-	// 	echo '<script type= "text/JavaScript"> alert("insert"); </script>'; 
-     //  $stmt->close();
-     //  $stmt = $conn->prepare($INSERT);
-     //  $stmt->bind_param("isi", $game, rithika, $score);
-     //  $stmt->execute();
-     //  echo '<script type= "text/JavaScript"> alert("insert"); </script>';
-	//   include_once('leaderboard.html');
-     // }
+	$stmt = $conn->prepare($SELECT);
+     $stmt->bind_param("s", rithika);
+     $stmt->execute();
+     $stmt->bind_result($user);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
+     if ($rnum==0) {
+		echo '<script type= "text/JavaScript"> alert("insert"); </script>'; 
+      $stmt->close();
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("isi", $game, rithika, $score);
+      $stmt->execute();
+      echo '<script type= "text/JavaScript"> alert("insert"); </script>';
+	  include_once('leaderboard.html');
+     }
+
+     echo "Reached 2nd";
 	//  else {
 	// $stmt = $conn->prepare($UPDATE);
      //  $stmt->bind_param("isi", $game, rithika, $score);
