cc641d50b67fcca2cb0a6ba1ea9380e320a48c69
jeyganesh7 <54430564+jeyganesh7@users.noreply.github.com>
2019-10-14 19:19:06 +1100

    test row numbers

--- testrank.php
+++ testrank.php
@@ -6,8 +6,10 @@ $sql = "SELECT username, gameScore, (SELECT COUNT(*)+1  FROM jeyganesh.score B W
 ORDER BY gamescore DESC";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
+$rnum = $result->num_rows;
 
 echo "Executed Statement";
+echo $rnum;
 echo $row['rank'];
 echo "Rank";
 ?>
