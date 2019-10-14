6b74958e88b4495800d6ddcadca495197e0008f2
jeyganesh7 <54430564+jeyganesh7@users.noreply.github.com>
2019-10-14 18:55:39 +1100

    test rank

--- testrank.php
+++ testrank.php
@@ -1,24 +1,14 @@
-<?php
-echo '<script type= "text/JavaScript"> alert("hi"); </script>';
-session_start();
+<?php include("dbconnection.php"); ?> 
 
-$rank = 0;
+<?php
+
 $sql = "SET @i=0;
 Select rank from (SELECT username, @i:=@i+1 AS rank 
  FROM jeyganesh.score where gameID = 1
  ORDER BY gamescore DESC) t where username = 'emily';";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
 
-echo "Statement created";
-
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-
-echo "Statement executed";
-
-$stmt->bind_result($rank);
-$stmt->store_result();
-$rnum = $stmt->num_rows;
-
-echo $rnum;
-echo "reached end";
+echo "Executed Statement";
+echo "<h3>" . $row["rank"] . "</h3>";
 ?>
