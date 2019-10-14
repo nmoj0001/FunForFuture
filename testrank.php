a8035c566ad50d71aad1c7696da250698de4c537
jeyganesh7 <54430564+jeyganesh7@users.noreply.github.com>
2019-10-14 19:25:33 +1100

    test

--- testrank.php
+++ testrank.php
@@ -5,17 +5,11 @@
 $sql = "SELECT (SELECT COUNT(*)+1  FROM jeyganesh.score B WHERE gameID = 1 and A.gamescore<B.gamescore) AS Rank FROM jeyganesh.score A where gameID = 1 and username = 'emily'
 ORDER BY gamescore DESC";
 
-$stmt = $conn->prepare($sql);
-$stmt->execute();
+$stmt = $conn->query($sql);
+$stmt = $result->fetch_assoc();
 $stmt->bind_result($rank);
 $stmt->store_result();
 $rnum = $stmt->num_rows;
-
-// $stmt = $conn->query($sql);
-// $stmt = $result->fetch_assoc();
-// $stmt->bind_result($rank);
-// $stmt->store_result();
-// $rnum = $result->num_rows;
 
 echo "Executed Statement";
 echo $rank;
